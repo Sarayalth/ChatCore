f79af6bec3bda10520a24e7caaa2390067df7e75
Eris <erisapps@outlook.be>
2020-10-15 21:27:41 +0200

    Renamed StreamCoreNotInitializedException to ChatCoreNotInitializedException

---
+++ ChatCore/Exceptions/ChatCoreNotInitializedException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ChatCore.Exceptions
+{
+    [Serializable]
+    public class ChatCoreNotInitializedException : Exception
+    {
+        public ChatCoreNotInitializedException()
+        {
+        }
+
+        public ChatCoreNotInitializedException(string message) : base(message)
+        {
+        }
+
+        public ChatCoreNotInitializedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ChatCoreNotInitializedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
