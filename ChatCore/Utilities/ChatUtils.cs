a3b503b683d5c4dca1d0042ee326a282ee3f0b52
Eris <erisapps@outlook.be>
2020-10-15 21:41:20 +0200

    Cleaned up Utilities classes

--- ChatCore/Utilities/ChatUtils.cs
+++ ChatCore/Utilities/ChatUtils.cs
@@ -6,52 +6,55 @@ using ChatCore.Services.Twitch;
 
 namespace ChatCore.Utilities
 {
-    public static class ChatUtils
-    {
-        public static TwitchService AsTwitchService(this IChatService svc)
-        {
-            return svc as TwitchService;
-        }
+	public static class ChatUtils
+	{
+		private static readonly ConcurrentDictionary<int, string> UserColors = new ConcurrentDictionary<int, string>();
 
-        public static TwitchMessage AsTwitchMessage(this IChatMessage msg)
-        {
-            return msg as TwitchMessage;
-        }
+		public static TwitchService? AsTwitchService(this IChatService svc)
+		{
+			return svc as TwitchService;
+		}
 
-        public static TwitchChannel AsTwitchChannel(this IChatChannel channel)
-        {
-            return channel as TwitchChannel;
-        }
+		public static TwitchMessage? AsTwitchMessage(this IChatMessage msg)
+		{
+			return msg as TwitchMessage;
+		}
 
-        public static TwitchUser AsTwitchUser(this IChatUser user)
-        {
-            return user as TwitchUser;
-        }
+		public static TwitchChannel? AsTwitchChannel(this IChatChannel channel)
+		{
+			return channel as TwitchChannel;
+		}
 
-        public static TwitchBadge AsTwitchBadge(this IChatBadge badge)
-        {
-            return badge as TwitchBadge;
-        }
+		public static TwitchUser? AsTwitchUser(this IChatUser user)
+		{
+			return user as TwitchUser;
+		}
 
-        public static TwitchEmote AsTwitchEmote(this IChatEmote emote)
-        {
-            return emote as TwitchEmote;
-        }
+		public static TwitchBadge? AsTwitchBadge(this IChatBadge badge)
+		{
+			return badge as TwitchBadge;
+		}
 
-        private static ConcurrentDictionary<int, string> _userColors = new ConcurrentDictionary<int, string>();
-        public static string GetNameColor(string name)
-        {
-            var nameHash = name.GetHashCode();
-            if (!_userColors.TryGetValue(nameHash, out var nameColor))
-            {
-                // Generate a psuedo-random color based on the users display name
-                var rand = new Random(nameHash);
-                var argb = (rand.Next(255) << 16) + (rand.Next(255) << 8) + rand.Next(255);
-                var colorString = string.Format("#{0:X6}FF", argb);
-                _userColors.TryAdd(nameHash, colorString);
-                nameColor = colorString;
-            }
-            return nameColor;
-        }
-    }
+		public static TwitchEmote? AsTwitchEmote(this IChatEmote emote)
+		{
+			return emote as TwitchEmote;
+		}
+
+		public static string GetNameColor(string name)
+		{
+			var nameHash = name.GetHashCode();
+			if (UserColors.TryGetValue(nameHash, out var nameColor))
+			{
+				return nameColor;
+			}
+
+			// Generate a psuedo-random color based on the users display name
+			var rand = new Random(nameHash);
+			var argb = (rand.Next(255) << 16) + (rand.Next(255) << 8) + rand.Next(255);
+			var colorString = $"#{argb:X6}FF";
+			UserColors.TryAdd(nameHash, colorString);
+			nameColor = colorString;
+			return nameColor;
+		}
+	}
 }
