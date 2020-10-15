417b0a1dd564390329c04134f224d967f4896436
Eris <erisapps@outlook.be>
2020-10-15 23:13:33 +0200

    General services cleanup

--- ChatCore/Services/DefaultBrowserLauncherService.cs
+++ ChatCore/Services/DefaultBrowserLauncherService.cs
@@ -1,18 +1,11 @@
 ﻿using ChatCore.Interfaces;
-using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 
 namespace ChatCore.Services
 {
     public class ProcessDotStartBrowserLauncherService : IDefaultBrowserLauncherService
     {
-        public ProcessDotStartBrowserLauncherService(ILogger<ProcessDotStartBrowserLauncherService> logger)
-        {
-            _logger = logger;
-        }
-        private ILogger _logger;
-
-        public void Launch(string uri)
+	    public void Launch(string uri)
         {
             Process.Start(uri);
         }
