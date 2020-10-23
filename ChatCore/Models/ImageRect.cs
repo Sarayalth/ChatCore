f52f0384ed0f0922bb3c6a84a6bf6c2914bab016
Eris <erisapps@outlook.be>
2020-10-23 19:25:38 +0200

    Fixed possible bug in ImageRect

--- ChatCore/Models/ImageRect.cs
+++ ChatCore/Models/ImageRect.cs
@@ -14,10 +14,10 @@ namespace ChatCore.Models
             var obj = JSON.Parse(json);
             if (obj != null)
             {
-                X = obj.TryGetKey(nameof(X), out var xJson) ? xJson.AsInt : 0;
-                Y = obj.TryGetKey(nameof(Y), out var yJson) ? yJson.AsInt : 0;
-                Width = obj.TryGetKey(nameof(Width), out var w) ? w.AsInt : 0;
-                Height = obj.TryGetKey(nameof(X), out var h) ? h.AsInt : 0;
+                X = obj.TryGetKey("x", out var xJson) ? xJson.AsInt : 0;
+                Y = obj.TryGetKey("y", out var yJson) ? yJson.AsInt : 0;
+                Width = obj.TryGetKey("width", out var wJson) ? wJson.AsInt : 0;
+                Height = obj.TryGetKey("height", out var hJson) ? hJson.AsInt : 0;
             }
 
             X = 0;
@@ -28,10 +28,10 @@ namespace ChatCore.Models
         public JSONObject ToJson()
         {
             var json = new JSONObject();
-            json.Add(nameof(X), new JSONNumber(X));
-            json.Add(nameof(Y), new JSONNumber(Y));
-            json.Add(nameof(Width), new JSONNumber(Width));
-            json.Add(nameof(Height), new JSONNumber(Height));
+            json.Add("x", new JSONNumber(X));
+            json.Add("y", new JSONNumber(Y));
+            json.Add("width", new JSONNumber(Width));
+            json.Add("height", new JSONNumber(Height));
             return json;
         }
 
