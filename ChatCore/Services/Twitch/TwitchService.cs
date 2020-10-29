0e428d3b86ab74a475e60e0abbb4f015c08c2136
Eris <erisapps@outlook.be>
2020-10-29 23:27:43 +0100

    Fixed encoding getting messed up
    Reordered usings while I was at it.

--- ChatCore/Services/Twitch/TwitchService.cs
+++ ChatCore/Services/Twitch/TwitchService.cs
@@ -1,15 +1,15 @@
-using ChatCore.Interfaces;
-using ChatCore.Models;
-using ChatCore.Models.Twitch;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
+using ChatCore.Interfaces;
+using ChatCore.Models;
+using ChatCore.Models.Twitch;
 using ChatCore.Utilities;
+using Microsoft.Extensions.Logging;
 
 namespace ChatCore.Services.Twitch
 {
